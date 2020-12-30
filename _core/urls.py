52b2a92b03a4dfa5c91f7df49de3d01bc28d0ec4
Endang.Ismaya <endang.ismaya@gmail.com>
2020-12-30 17:32:52 +0700

    create templates for every render...

--- _core/urls.py
+++ _core/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
-]
+    path('', include('blog.urls', namespace='blog')),
+    path('accounts/login', views.login, name='login'),
+    path('áccount/logout', views.logout, name='logut', kwargs={'next_page': '/'})
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
